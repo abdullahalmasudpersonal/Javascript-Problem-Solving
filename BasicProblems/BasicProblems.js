8f523773aae17e4b73b193d3b67c9f569e5e94d3
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-09-23 01:13:58 +0600

    --Object Manipulation-- Create An Array Of Objects, Representing Books With Properties Like Title, Author, And Year. Write A Function That Takes The Array And Returns A New Array With Only The Book Titles. Print The Result.

--- BasicProblems/BasicProblems.js
+++ BasicProblems/BasicProblems.js
@@ -1,4 +1,4 @@
-//   1.Task: Array Filtering And Mapping
+//  problem no 01 --- Array Filtering And Mapping
 
 // Create An Array Of Objects, Each Representing A Person With Properties Like Name, Age, And Gender. Write A Function To Filter Out All Females And Then Map The Remaining People To An Array Of Names. Print The Final Result.
 
@@ -20,3 +20,25 @@ function femaleFilterAndMapRemainingPeople(people) {
 
 const result = femaleFilterAndMapRemainingPeople(peoples);
 console.log("Problem-No-01", result);
+
+
+/* problem no 02 --- Object Manipulation---  */
+
+/// Create An Array Of Objects, Representing Books With Properties Like Title, Author, And Year. Write A Function That Takes The Array And Returns A New Array With Only The Book Titles. Print The Result.
+
+
+
+const books = [
+    { title: "Boker Vetor Nodi", author: "Humayun Ahmed", year: 1990 },
+    { title: "Putul Nacher Itikotha", author: "Manik Bandopadhyay", year: 1936 },
+    { title: "Saatkahon", author: "Samaresh Majumdar", year: 1981 },
+    { title: "Kobi", author: "Tarashankar Bandopadhyay", year: 1949 },
+    { title: "Lalsalu", author: "Syed Waliullah", year: 1948 },
+];
+
+function getBookTitles(book) {
+    return book.map((book) => book.title);
+}
+
+const titles = getBookTitles(books);
+console.log("Problem-No-02", titles);
